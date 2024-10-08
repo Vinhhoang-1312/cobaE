a2fccd4f2be8232ba01347908cd96c8c26a13fd7
vinhhoang-1312 <tranphanvinhhoang7@gmail.com>
2024-10-08 19:58:16 +0700

    chỉnh start script de deploy client

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://coba-mart.herokuapp.com/api/";
-const BASE_URL = "http://localhost:5000/api/";
+// const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://cobae-client.onrender.com";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
